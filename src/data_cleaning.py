37b33fc935feffea1acdf1c05e0ba72192832877
TylerLott <tylerlott0126@gmail.com>
2022-07-03 00:21:45 -0600

    data cleaning work

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,3 +1,4 @@
+import hashlib
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -6,7 +7,7 @@ from pathlib import Path
 def clean_violations(df: pd.DataFrame) -> pd.DataFrame:
     """
     This function cleans the 'Violations' columns of the main df. It splits the information out
-    into 3 different columns ['v_num', 'v_comment', 'v_desc']. It also splits violations into
+    into 3 different columns ['number', 'comment', 'desc']. It also splits violations into
     their own distinct rows
 
     Args:
@@ -16,12 +17,12 @@ def clean_violations(df: pd.DataFrame) -> pd.DataFrame:
         pd.DataFrame: dataframe with 3 new columns
     """
     # convert violations to lowercase
-    df["Violations"] = df["Violations"].str.lower()
     mask = df["Violations"].notna()
+    df[mask, "Violations"] = df[mask, "Violations"].str.lower()
     # create violation number column
     num_reg = r"(?:^|\| )(\d+\. )"
     vio_nums = df.loc[mask, "Violations"].str.extractall(num_reg)
-    df.loc[mask, "v_num"] = vio_nums.groupby(level=0)[0].apply(list)
+    df.loc[mask, "number"] = vio_nums.groupby(level=0)[0].apply(list)
     # create violation_temp
     split_reg = r"(?:^|\| )\d+\. "
     df.loc[mask, "vio_temp"] = (
@@ -30,16 +31,16 @@ def clean_violations(df: pd.DataFrame) -> pd.DataFrame:
         .apply(lambda x: x[1:])
     )
     # split into individual rows for violations
-    df = df.explode(["vio_temp", "v_num"]).reset_index(drop=True)
+    df = df.explode(["vio_temp", "number"]).reset_index(drop=True)
     exp_mask = df["Violations"].notna()
     # create comment and desc violation columns
     desc_split_reg = "- comments:"
-    df.loc[exp_mask, "v_comment"] = (
+    df.loc[exp_mask, "comment"] = (
         df.loc[exp_mask, "vio_temp"]
         .str.split(desc_split_reg)
         .apply(lambda x: x[-1] if len(x) > 1 else np.nan)
     )
-    df.loc[exp_mask, "v_desc"] = (
+    df.loc[exp_mask, "desc"] = (
         df.loc[exp_mask, "vio_temp"].str.split(desc_split_reg).apply(lambda x: x[0])
     )
     df = df.drop(columns=["vio_temp", "Violations"])
@@ -54,6 +55,17 @@ def clean_addresses(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def create_mappings(df: pd.DataFrame) -> pd.DataFrame:
+    df["RestaurantId"] = df.apply(
+        lambda x: abs(
+            hash(str(x["DBA Name"]) + str(x["AKA Name"]) + str(x["Facility Type"]))
+        )
+        % (10**6),
+        axis=1,
+    )
+    return df
+
+
 if __name__ == "__main__":
     ##########################################
     # import data
@@ -65,46 +77,27 @@ if __name__ == "__main__":
     ##########################################
     # Clean
     ##########################################
-    INSPECTION_COLS = [
-        "Inspection ID",
-        "License #",
-        "Inspection Date",
-        "Inspection Type",
-        "Risk",
-        "Results",
-        "Violations",
-    ]
-    RESTAURANT_COLS = ["DBA Name", "AKA Name", "Facility Type"]
-    ADDRESSES_COLS = [
-        "Address",
-        "City",
-        "State",
-        "Zip",
-        "Location",
-        "Latitude",
-        "Longitude",
-    ]
-    # clean inspections table info
-    inspections_df = og_df[INSPECTION_COLS]
-    inspections_df = clean_violations(inspections_df)
-
+    # clean the violations
+    inter_df = clean_violations(og_df)
     # clean restaurant table info
-    rest_df = og_df[RESTAURANT_COLS]
-    rest_df = clean_restaurants(rest_df)
-
+    inter_df = clean_restaurants(inter_df)
     # clean addresses df
-    add_df = og_df[ADDRESSES_COLS]
-    add_df = clean_addresses(add_df)
+    inter_df = clean_addresses(inter_df)
+    # create columns for mapping between tables
+    clean_df = create_mappings(inter_df)
 
     ##########################################
     # Save DFs
     ##########################################
+    INS_COLS = []
+    REST_COLS = []
+    ADD_COLS = []
     # save off inspections df
     inspections_path = data_path / "inspection_table_df.csv"
-    inspections_df.to_csv(inspections_path)
+    clean_df.to_csv(inspections_path)
     # save off restaurants df
     rest_path = data_path / "restaurant_table_df.csv"
-    rest_df.to_csv(rest_path)
+    clean_df.to_csv(rest_path)
     # save off addresses df
     add_path = data_path / "addresses_table_df.csv"
-    add_df.to_csv(add_path)
+    clean_df.to_csv(add_path)
